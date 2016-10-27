c4a0c9cf19db179ec8e21ab80d1c2a84b6a7bd02
EnderS <raphvdm@gmail.com>
2016-10-27 16:00:13 +0200

    jar created

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import java.io.*;
 
 public class Main {
-public static void main(String[] args){
-	Symbol sym;
-	LexicalAnalyzer lexer = new LexicalAnalyzer(new FileReader(args[0]));
-	for (sym = lexer.next_token(); sym.sym != 0; sym = lexer.next_token())
-		System.out.println(sym);
-}
+	/*
+	public static void main(String[] args){
+		Symbol sym;
+		LexicalAnalyzer lexer = new LexicalAnalyzer(new FileReader(args[0]));
+		for (sym = lexer.next_token(); sym.sym != 0; sym = lexer.next_token())
+			System.out.println(sym);
+	}
+	*/
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
 }
