c1f12d89fe5641f01d63e212b86852aed4e60ad1
acaccia <acaccia@ulb.ac.be>
2016-11-09 09:47:55 +0100

    no more standalone required \o/

--- more/src/Main.java
+++ more/src/Main.java
@@ -1,5 +1,16 @@
+import java.io.*;
+
 public class Main {
 public static void main (String[] args){
-	LexicalAnalyzer.main(args);
+	try {
+		LexicalAnalyzer la = new LexicalAnalyzer(new FileReader(args[0]));
+		Symbol sym = la.yylex();
+		while (sym != null) {
+			System.out.println(sym);
+			sym = la.yylex();
+		}
+	} catch (IOException e) {
+		System.out.println("File Not Found: " + args[0]);
+	}
 }
 }
