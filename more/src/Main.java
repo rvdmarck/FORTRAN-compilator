a03820ba73ba3487d9ef49398742ef989fdb22ad
acaccia <acaccia@ulb.ac.be>
2016-12-03 16:37:30 +0100

    main function updated

--- more/src/Main.java
+++ more/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
 public static void main (String[] args){
 	try {
 		LexicalAnalyzer la = new LexicalAnalyzer(new FileReader(args[0]));
-		for (Symbol sym = la.yylex(); sym != null; sym = la.yylex())
-			System.out.println(sym);
+		Parser p = new Parser(la);
+		p.run();
 	} catch (IOException e) {
 		System.out.println("File Not Found: " + args[0]);
+	} catch (ParserException e) {
+		System.err.println(e);
 	}
 }
 }
